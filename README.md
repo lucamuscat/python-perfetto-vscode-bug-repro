b485153d3b9ee4e3e037bae7492751e75907e008
Luca Muscat <lucamuscat@gmail.com>
2024-12-08 11:30:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Setup
+The project is managed using [poetry, the python build & dependency manager](https://python-poetry.org/).
+
+## Setup using poetry
+Run `poetry install` to install perfetto.
+
+## Setup using pip
+If you do not wish to install poetry on your machine, make sure to `pip install -r requirements.txt`.
---
+++ main.py
@@ -0,0 +1,14 @@
+import perfetto
+import perfetto.trace_processor
+import sys
+
+def log_popen_invocations(event_name: str, args) -> None:
+    """Print all invocations of popen to help with figuring out why `load_shell` is being given
+    invalid arguments"""
+    if event_name == "subprocess.Popen":
+        print(event_name, args)
+    
+sys.addaudithook(log_popen_invocations)
+
+with perfetto.trace_processor.TraceProcessor(trace="trace.json") as trace_processor:
+    trace_processor.query("select * from slices")
